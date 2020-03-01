f7a45e5e4edbd31e3a127a251a2fdf9a0ce26ced
raylexlee <raylexym57lee@gmail.com>
2020-03-02 00:16:57 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const {app, BrowserWindow} = require('electron') 
+const url = require('url') 
+const path = require('path')  
+const fs = require('fs');
+
+let win  
+
+function createWindow() { 
+   win = new BrowserWindow({width: 1280, height: 700}) 
+   win.loadURL(url.format ({ 
+      pathname: path.join(__dirname, 'index.html'), 
+      protocol: 'file:', 
+      slashes: true 
+   })) 
+//   win.webContents.openDevTools()
+// "floating" + 1 is higher than all regular windows, but still behind things 
+// like spotlight or the screen saver
+//   win.setAlwaysOnTop(true, "floating", 1);
+// allows the window to show over a fullscreen window
+//   win.setVisibleOnAllWorkspaces(true);
+
+}  
+
+app.on('ready', createWindow) 
