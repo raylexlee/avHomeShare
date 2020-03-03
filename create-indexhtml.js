c18d177c041bc0fa37d218b452ad733fda365c73
raylexlee <raylexym57lee@gmail.com>
2020-03-03 18:55:56 +0800

    Update with encodeURIcomponent()

--- create-indexhtml.js
+++ create-indexhtml.js
@@ -1,17 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
-const videoHtml = (subdir, cover, movie) => `<video controls width="auto" height="100%" poster="movies/${subdir}/${cover}">
+const videoHtml = (subdir, cover, movie) => {
+   subdir = encodeURIComponent(subdir);
+   cover = encodeURIComponent(cover);
+   movie = encodeURIComponent(movie);
+   return `<video controls width="auto" height="95%" poster="movies/${subdir}/${cover}" onended="this.load();">
    <source src="movies/${subdir}/${movie}" type="video/mp4" />
 </video>
-`;
+`; }
 
 const folders = fs.readdirSync(path.join(__dirname, 'movies'), {encoding: "utf8", withFileTypes: true}).filter(f => f.isDirectory);
 module.exports = folders.map(folder => {
   const files = fs.readdirSync(path.join(__dirname, 'movies', folder.name));
   const cover = files.filter(file => file.split('.').pop() === 'jpg');
   const movie = files.filter(file => file.split('.').pop() === 'mp4');
-  return (cover.length === 1 && movie.length === 1) ? videoHtml(folder.name, cover[0], movie[0]) : '';
+  return (cover.length === 1 && movie.length === 1) 
+    ? videoHtml(folder.name, cover[0], movie[0]) 
+    : '';
   }).join('\n');
 
   
