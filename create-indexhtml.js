e2f0bf6a1c9d26c58f91c2047e675f42c2a3e4d1
raylexlee <raylexym57lee@gmail.com>
2020-03-09 19:14:56 +0800

    Tweak video inline css and remove slider thought

--- create-indexhtml.js
+++ create-indexhtml.js
@@ -5,13 +5,23 @@ const videoHtml = (subdir, cover, movie) => {
    subdir = encodeURIComponent(subdir);
    cover = encodeURIComponent(cover);
    movie = encodeURIComponent(movie);
-   return `<video controls width="auto" height="95%" poster="movies/${subdir}/${cover}" onended="this.load();">
+   return `<video controls poster="movies/${subdir}/${cover}" onended="this.load();">
    <source src="movies/${subdir}/${movie}" type="video/mp4" />
 </video>
 `; }
+const indexHtml = (videos) => `<style>
+  video {
+    object-fit: contain;
+    max-height: 100vh;
+    width: 100%;
+    height: auto;
+    }
+</style>
+${videos}
+`;
 
 const folders = fs.readdirSync(path.join(__dirname, 'movies'), {encoding: "utf8", withFileTypes: true}).filter(f => f.isDirectory);
-module.exports = folders.map(folder => {
+const Videos = folders.map(folder => {
   const files = fs.readdirSync(path.join(__dirname, 'movies', folder.name));
   const cover = files.filter(file => file.split('.').pop() === 'jpg');
   const movie = files.filter(file => file.split('.').pop() === 'mp4');
@@ -19,5 +29,4 @@ module.exports = folders.map(folder => {
     ? videoHtml(folder.name, cover[0], movie[0]) 
     : '';
   }).join('\n');
-
-  
+module.exports = indexHtml(Videos);  
